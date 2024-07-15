fb8cdf2827938cb88dd92e591e4d24d1a62f4004
Taranpreet Singh <issar.taran@gmail.com>
2024-07-16 02:31:29 +0530

    -error handler

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -15,7 +15,31 @@ module.exports = (err, req, res, next) => {             //format of RESTful API
         const message =` Resource not found. Invalid: ${err.path}`;
         err = new ErrorHandler(message,400);
     }
+
+
+//duplicate error for email register
+    if(err.code === 11000){
+        const message =` Duplicate ${Object.keys(err.keyValue)} found`;
+        err = new ErrorHandler(message,400);
+    }
+
+
+    if(err.name === "JsonWebTokenError"){
+        const message =`Json Web token is invalid ,Try again`;
+        err = new ErrorHandler(message,400);
+    }
     
+    
+    if(err.name === "TokenExpiredError"){
+        const message =`Json Web token is expired,Try again`;
+        err = new ErrorHandler(message,400);
+    }
+    
+
+
+
+
+
     res.status(err.statusCode).json({ 
         success: false,
         // error: err.stack,               //stack is basically give the errors exact location
